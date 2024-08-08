28d57346900c2ed9ebecff0a62f16254854933b3
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-08-08 14:47:08 -0400

    fix stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿// a method that check a stack for it's info.
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count > 0)
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        else
+            Console.WriteLine("Stack is empty");
+
+        Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Count > 0 && aStack.Pop() != search)
+            {
+
+            }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
