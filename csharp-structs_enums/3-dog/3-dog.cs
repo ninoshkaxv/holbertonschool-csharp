1d0292862188aa1851e5aa6359022a25507e1169
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-08-21 14:28:18 -0400

    add Readme.md file and tasks 0-3

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog( string name, float age, string owner, Rating rate )
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rate;
+    }
+
+    public override string ToString()
+    {
+        return (String.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}",this.name, this.age, this.owner, this.rating));
+    }
+}
