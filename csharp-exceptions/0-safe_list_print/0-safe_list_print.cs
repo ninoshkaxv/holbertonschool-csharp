e843b690e550ae05f1385b9d37dc0a84a4d08aed
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-08-21 12:55:19 -0400

    add Readme.md file and 0-1 task

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printedCount = 0;
+        
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                printedCount++;
+            }
+        }
+        catch(Exception)
+        {
+            ;
+        }
+        
+        return printedCount;
+    }
+}
