273680ddc1230dc5670617fd84c1374ac9907c6a
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-08-28 20:01:59 -0400

    define class

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class creates a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
